b2ce79a275d39f3e856b9c5eb758dd50be48b828
GSerjo <sedot8@gmail.com>
2013-09-07 14:52:38 +0400

    + Sorter.cs algorithms for sorting + SorterTests.cs

---
+++ Source/Algorithms/Sorter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public static class Sorter
+    {
+        public static List<int> Selection(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[j] < list[min])
+                    {
+                        min = j;
+                    }
+                }
+                int dummy = list[i];
+                list[i] = list[min];
+                list[min] = dummy;
+            }
+            return list;
+        }
+    }
+}
---
+++ Source/UnitTests/Algorithms/SorterTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Algorithms;
+using Xunit;
+
+namespace UnitTests.Algorithms
+{
+    public sealed class SorterTests
+    {
+        [Fact]
+        public void Selection()
+        {
+            var source = new List<int> { 3, 5, 1, 0, 7 };
+            var expected = new List<int> { 0, 1, 3, 5, 7 };
+            List<int> actual = Sorter.Selection(source);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
