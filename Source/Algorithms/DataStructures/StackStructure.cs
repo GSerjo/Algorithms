f9be1c3546a835df11fd91852e9569b40aaf1cd7
GSerjo <sedot8@gmail.com>
2013-12-26 00:41:42 +0400

    + StackStructure.cs non thread safe

---
+++ Source/Algorithms/DataStructures/StackStructure.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms.DataStructures
+{
+    public class StackStructure<T>
+    {
+        private readonly Node<T> _head = new Node<T>();
+
+        public T Pop()
+        {
+            Node<T> node = _head.Next;
+            if (node == null)
+            {
+                return default(T);
+            }
+            _head.Next = node.Next;
+            return node.Data;
+        }
+
+        public void Push(T data)
+        {
+            var node = new Node<T> { Data = data, Next = _head.Next };
+            _head.Next = node;
+        }
+
+        private sealed class Node<TData>
+        {
+            public TData Data { get; set; }
+            public Node<TData> Next { get; set; }
+        }
+    }
+}
