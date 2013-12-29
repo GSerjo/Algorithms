f7e7acacb30645cb9568eff1c62578a579f4b4dd
GSerjo <sedot8@gmail.com>
2013-12-30 00:37:13 +0400

    + LinkedListStructure.cs initial version (only AddFirst)

---
+++ Source/Algorithms/DataStructures/LinkedListStructure.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace Algorithms.DataStructures
+{
+    public sealed class LinkedListStructure<T>
+    {
+        private readonly Node<T> _head = new Node<T>();
+        private readonly Node<T> _tail = new Node<T>();
+
+        public LinkedListStructure()
+        {
+            _head = _tail;
+        }
+
+        public int Count { get; private set; }
+
+        public LinkedListStructure<T> AddFirst(T value)
+        {
+            var node = new Node<T> { Value = value, Next = _head.Next };
+            _head.Next = node;
+            Count++;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder("{");
+            Node<T> node = _head.Next;
+            while (node != null)
+            {
+                builder.AppendFormat("{0};", node.Value);
+                node = node.Next;
+            }
+            builder.Append("}");
+            return builder.ToString();
+        }
+
+        private sealed class Node<TValue>
+        {
+            public Node<TValue> Next { get; set; }
+            public TValue Value { get; set; }
+        }
+    }
+}
---
+++ Source/UnitTests/DataStructures/LinkedListStructureTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Algorithms.DataStructures;
+using Xunit;
+
+namespace UnitTests.DataStructures
+{
+    public sealed class LinkedListStructureTest
+    {
+        [Fact]
+        public void AddFirst()
+        {
+            var linkedList = new LinkedListStructure<int>();
+            linkedList
+                .AddFirst(0)
+                .AddFirst(1)
+                .AddFirst(2);
+            Assert.Equal(3, linkedList.Count);
+            Console.WriteLine(linkedList.ToString());
+        }
+    }
+}
