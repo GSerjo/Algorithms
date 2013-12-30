4da92b4986271d12ba32785a43cf3daa2bf7f619
GSerjo <sedot8@gmail.com>
2013-12-31 02:17:33 +0400

    * LinkedListStructure.cs added Clear, Contains, Enumerator

--- Source/Algorithms/DataStructures/LinkedListStructure.cs
+++ Source/Algorithms/DataStructures/LinkedListStructure.cs
@@ -1,32 +1,45 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Algorithms.DataStructures
 {
-    public sealed class LinkedListStructure<T>
+    public sealed class LinkedListStructure<T> : ICollection<T>
     {
         private readonly Node<T> _head = new Node<T>();
         private readonly Node<T> _tail = new Node<T>();
 
-        public LinkedListStructure()
-        {
-            _head = _tail;
-        }
-
         public int Count { get; private set; }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
 
         public LinkedListStructure<T> AddFirst(T value)
         {
             var node = new Node<T> { Value = value, Next = _head.Next };
-            _head.Next = node;
             Count++;
+            if (Count == 1)
+            {
+                _tail.Next = node;
+            }
+            _head.Next = node;
             return this;
         }
 
         public LinkedListStructure<T> AddLast(T value)
         {
             var node = new Node<T> { Value = value };
-            Node<T> previousNode = _tail.Next;
-            previousNode.Next = node;
+            Count++;
+            if (Count == 1)
+            {
+                _head.Next = node;
+                _tail.Next = node;
+                return this;
+            }
+            _tail.Next.Next = node;
             _tail.Next = node;
             return this;
         }
@@ -42,6 +55,57 @@ namespace Algorithms.DataStructures
             }
             builder.Append("}");
             return builder.ToString();
+        }
+
+        public void Add(T item)
+        {
+            AddFirst(item);
+        }
+
+        public void Clear()
+        {
+            _head.Next = null;
+            _tail.Next = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            Node<T> node = _head.Next;
+            while (node != null)
+            {
+                if (node.Value.Equals(item))
+                {
+                    return true;
+                }
+                node = node.Next;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> node = _head.Next;
+            while (node.Next != null)
+            {
+                yield return node.Value;
+                node = node.Next;
+            }
         }
 
         private sealed class Node<TValue>
--- Source/UnitTests/DataStructures/LinkedListStructureTest.cs
+++ Source/UnitTests/DataStructures/LinkedListStructureTest.cs
@@ -15,7 +15,68 @@ namespace UnitTests.DataStructures
                 .AddFirst(1)
                 .AddFirst(2);
             Assert.Equal(3, linkedList.Count);
+            Assert.Equal("{2;1;0;}", linkedList.ToString());
             Console.WriteLine(linkedList.ToString());
+        }
+
+        [Fact]
+        public void AddFirstAndLast()
+        {
+            var linkedList = new LinkedListStructure<int>();
+            linkedList
+                .AddFirst(0)
+                .AddLast(2)
+                .AddFirst(1);
+            Assert.Equal(3, linkedList.Count);
+            Assert.Equal("{1;0;2;}", linkedList.ToString());
+        }
+
+        [Fact]
+        public void AddLast()
+        {
+            var linkedList = new LinkedListStructure<int>();
+            linkedList
+                .AddLast(0)
+                .AddLast(1)
+                .AddLast(2);
+            Assert.Equal(3, linkedList.Count);
+            Assert.Equal("{0;1;2;}", linkedList.ToString());
+            Console.WriteLine(linkedList.ToString());
+        }
+
+        [Fact]
+        public void Clear()
+        {
+            var linkedList = new LinkedListStructure<int>();
+            linkedList
+                .AddFirst(0)
+                .AddFirst(1);
+            linkedList.Clear();
+            Assert.Equal(0, linkedList.Count);
+        }
+
+        [Fact]
+        public void Contains_Absent_False()
+        {
+            var linkedList = new LinkedListStructure<int>();
+            linkedList
+                .AddFirst(0)
+                .AddFirst(1)
+                .AddFirst(2);
+            bool contains = linkedList.Contains(3);
+            Assert.False(contains);
+        }
+
+        [Fact]
+        public void Contains_Exist_True()
+        {
+            var linkedList = new LinkedListStructure<int>();
+            linkedList
+                .AddFirst(0)
+                .AddFirst(1)
+                .AddFirst(2);
+            bool contains = linkedList.Contains(1);
+            Assert.True(contains);
         }
     }
 }
