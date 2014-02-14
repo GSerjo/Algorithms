f19d675eef2bf8264a73c7007f6ead460856de6d
GSerjo <sedot8@gmail.com>
2014-02-14 16:49:06 +0400

    + CircularLinkedListStructureTest

--- Source/Algorithms/DataStructures/CircularLinkedListStructure.cs
+++ Source/Algorithms/DataStructures/CircularLinkedListStructure.cs
@@ -1,8 +1,11 @@
-﻿namespace Algorithms.DataStructures
+﻿using System.Text;
+
+namespace Algorithms.DataStructures
 {
     public class CircularLinkedListStructure<T>
     {
         private Node _tail;
+        public int Count { get; private set; }
 
         public CircularLinkedListStructure<T> AddFirst(T value)
         {
@@ -17,7 +20,21 @@
                 node.Next = _tail.Next;
                 _tail.Next = node;
             }
+            Count++;
             return this;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder("{");
+            Node node = _tail.Next;
+            do
+            {
+                builder.AppendFormat("{0};", node.Value);
+                node = node.Next;
+            } while (node != _tail.Next);
+            builder.Append("}");
+            return builder.ToString();
         }
 
         private sealed class Node
