40b0e3cfb7de9e6f080ccdd76fa8b204f4126ee2
GSerjo <sedot8@gmail.com>
2014-02-14 16:13:27 +0400

    + CircularLinkedListStructure initial version

---
+++ Source/Algorithms/DataStructures/CircularLinkedListStructure.cs
@@ -0,0 +1,29 @@
+﻿namespace Algorithms.DataStructures
+{
+    public class CircularLinkedListStructure<T>
+    {
+        private Node _tail;
+
+        public CircularLinkedListStructure<T> AddFirst(T value)
+        {
+            var node = new Node { Value = value };
+            if (_tail == null)
+            {
+                _tail = node;
+                _tail.Next = node;
+            }
+            else
+            {
+                node.Next = _tail.Next;
+                _tail.Next = node;
+            }
+            return this;
+        }
+
+        private sealed class Node
+        {
+            public Node Next { get; set; }
+            public T Value { get; set; }
+        }
+    }
+}
