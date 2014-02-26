0c1d1262072a0bf71df31b4b7ecd3e423b847dae
GSerjo <sedot8@gmail.com>
2014-02-26 22:51:35 +0400

    + TwoSum.cs with quadratic order of growth

---
+++ Source/Algorithms/RobertSedgewick/Analysis/TwoSum.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Algorithms.RobertSedgewick.Analysis
+{
+    public sealed class TwoSum
+    {
+        public static int Slow(string sourceDataPath)
+        {
+            int result = 0;
+            List<int> source = Data(sourceDataPath);
+            for (int i = 0; i < source.Count; i++)
+            {
+                for (int j = i + 1; j < source.Count; j++)
+                {
+                    if (source[i] + source[j] == 0)
+                    {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static List<int> Data(string sourceDataPath)
+        {
+            return File.ReadLines(sourceDataPath)
+                       .Select(int.Parse)
+                       .ToList();
+        }
+    }
+}
---
+++ Source/UnitTests/RobertSedgewick/Analysis/TwoSumTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using Algorithms.RobertSedgewick.Analysis;
+using Xunit.Extensions;
+
+namespace UnitTests.RobertSedgewick.Analysis
+{
+    public sealed class TwoSumTest
+    {
+        [InlineData(@"DataStore\14analysis\1Kints.txt")]
+        [InlineData(@"DataStore\14analysis\2Kints.txt")]
+        [InlineData(@"DataStore\14analysis\4Kints.txt")]
+        [InlineData(@"DataStore\14analysis\8Kints.txt")]
+        [InlineData(@"DataStore\14analysis\16Kints.txt")]
+        [Theory]
+        public void Slow(string path)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            int result = TwoSum.Slow(path);
+            stopwatch.Stop();
+            Console.WriteLine(
+                "File: {0} Time: {1} ms Result: {2}", path, stopwatch.Elapsed.TotalMilliseconds, result);
+        }
+    }
+}
