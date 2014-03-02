44bd31dd7f7ac9885cf8319e13601cdcc401a064
GSerjo <sedot8@gmail.com>
2014-03-03 01:26:31 +0400

    * TwoSumTest.cs added performance measurement

--- Source/UnitTests/RobertSedgewick/Analysis/TwoSumTest.cs
+++ Source/UnitTests/RobertSedgewick/Analysis/TwoSumTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using Algorithms.RobertSedgewick.Analysis;
+using Xunit;
 using Xunit.Extensions;
 
 namespace UnitTests.RobertSedgewick.Analysis
@@ -25,6 +27,25 @@ namespace UnitTests.RobertSedgewick.Analysis
                 "File: {0} Time: {1} ms Result: {2}", path, stopwatch.Elapsed.TotalMilliseconds, result);
         }
 
+        [Fact]
+        public void Quick_MeasurePerformance()
+        {
+            int counter = 0;
+            TimeSpan previous = Measure(TwoSum.Quick, Data(150));
+            for (int i = 250;; i += i)
+            {
+                TimeSpan current = Measure(TwoSum.Quick, Data(i));
+                double ratio = current.TotalMilliseconds/previous.TotalMilliseconds;
+                Console.WriteLine("DataLength: {0}, Time: {1} ms Ratio: {2}", i, current.TotalMilliseconds, ratio);
+                previous = current;
+                counter++;
+                if (counter >= 10)
+                {
+                    break;
+                }
+            }
+        }
+
         [InlineData(@"DataStore\14analysis\1Kints.txt")]
         [InlineData(@"DataStore\14analysis\2Kints.txt")]
         [InlineData(@"DataStore\14analysis\4Kints.txt")]
@@ -40,11 +61,46 @@ namespace UnitTests.RobertSedgewick.Analysis
                 "File: {0} Time: {1} ms Result: {2}", path, stopwatch.Elapsed.TotalMilliseconds, result);
         }
 
+        [Fact]
+        public void Slow_MeasurePerformance()
+        {
+            int counter = 0;
+            TimeSpan previous = Measure(TwoSum.Slow, Data(150));
+            for (int i = 250;; i += i)
+            {
+                TimeSpan current = Measure(TwoSum.Slow, Data(i));
+                double ratio = current.TotalMilliseconds/previous.TotalMilliseconds;
+                Console.WriteLine("DataLength: {0}, Time: {1} ms Ratio: {2}", i, current.TotalMilliseconds, ratio);
+                previous = current;
+                counter++;
+                if (counter >= 8)
+                {
+                    break;
+                }
+            }
+        }
+
         private static List<int> Data(string sourceDataPath)
         {
             return File.ReadLines(sourceDataPath)
                        .Select(int.Parse)
                        .ToList();
+        }
+
+        private static List<int> Data(int count)
+        {
+            var data = new byte[count];
+            var provider = new RNGCryptoServiceProvider();
+            provider.GetBytes(data);
+            return data.Select(Convert.ToInt32).ToList();
+        }
+
+        private TimeSpan Measure(Func<List<int>, int> func, List<int> data)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            func(data);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
         }
     }
 }
