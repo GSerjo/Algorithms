7641bb7527d5fa2cc4dfa154f4ede73c4b03a31d
GSerjo <sedot8@gmail.com>
2014-03-16 10:54:32 +0400

    + LinkedListQueue.cs

---
+++ Source/Algorithms/RobertSedgewick/DataStructures/LinkedListQueue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Algorithms.RobertSedgewick.DataStructures
+{
+    /// <summary>
+    ///     FIFO
+    /// </summary>
+    public sealed class LinkedListQueue<TItem>
+    {
+        private int _capacity;
+        private Node _head;
+        private Node _tail;
+
+        public int Count
+        {
+            get { return _capacity; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return _head == null; }
+        }
+
+        public TItem Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            TItem item = _head.Item;
+            _head = _head.Next;
+            if (IsEmpty)
+            {
+                _tail = null;
+            }
+            _capacity--;
+            return item;
+        }
+
+        public void Enqueue(TItem item)
+        {
+            Node previouseTail = _tail;
+            _tail = new Node
+                {
+                    Item = item,
+                };
+            if (IsEmpty)
+            {
+                _head = _tail;
+            }
+            else
+            {
+                previouseTail.Next = _tail;
+            }
+            _capacity++;
+        }
+
+        private sealed class Node
+        {
+            public TItem Item { get; set; }
+            public Node Next { get; set; }
+        }
+    }
+}
