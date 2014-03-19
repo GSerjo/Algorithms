edf6cd099a0aa8ae45bed43885d6629cd9b8812e
GSerjo <sedot8@gmail.com>
2014-03-20 01:07:23 +0400

    + InsertionSorter.cs + InsertionSorterTest.cs

---
+++ Source/Algorithms/RobertSedgewick/Sorting/InsertionSorter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algorithms.RobertSedgewick.Sorting
+{
+    public sealed class InsertionSorter : Sorter
+    {
+        public static void Sort(IComparable[] value)
+        {
+            for (int i = 1; i < value.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (Less(value[j], value[j - 1]))
+                    {
+                        Exchange(value, j, j - 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/UnitTests/RobertSedgewick/Sorting/InsertionSorterTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Algorithms.RobertSedgewick.Sorting;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.Sorting
+{
+    public sealed class InsertionSorterTest
+    {
+        [Fact]
+        public void Sort()
+        {
+            var array = new IComparable[] { 2, 0, 3, 4, 6, 5 };
+            InsertionSorter.Sort(array);
+            Assert.True(Sorter.IsSorted(array));
+        }
+    }
+}
