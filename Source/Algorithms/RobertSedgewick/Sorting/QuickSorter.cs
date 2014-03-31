2299dd39614a96e367e9b17056b6d426a092a8e0
GSerjo <sedot8@gmail.com>
2014-04-01 00:31:53 +0400

    + QuickSorter.cs initial version

---
+++ Source/Algorithms/RobertSedgewick/Sorting/QuickSorter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Algorithms.RobertSedgewick.Sorting
+{
+    public sealed class QuickSorter : Sorter
+    {
+        public static IComparable[] Sort(IComparable[] value)
+        {
+            Sort(value, 0, value.Length);
+            return value;
+        }
+
+        private static int Partition(IComparable[] value, int start, int end)
+        {
+            int i = start;
+            int hi = value.Length == end ? end - 1 : end;
+            for (int j = i; j < hi; j++)
+            {
+                if (Less(value[j], value[hi]))
+                {
+                    Exchange(value, j, hi);
+                    i++;
+                }
+            }
+            if (end == value.Length)
+            {
+                return i;
+            }
+            Exchange(value, end, i);
+            return i;
+        }
+
+        private static void Sort(IComparable[] value, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            int pivot = Partition(value, start, end);
+            Sort(value, start, pivot - 1);
+            Sort(value, pivot + 1, end);
+        }
+    }
+}
--- Source/UnitTests/ForTest.cs
+++ Source/UnitTests/ForTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System;
+
+using Xunit;
 
 namespace UnitTests
 {
