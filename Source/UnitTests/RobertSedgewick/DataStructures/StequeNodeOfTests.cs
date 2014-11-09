91c31b8772fc98e4eb02bfd8e9f2fca4fa047a4e
GSerjo <sedot8@gmail.com>
2014-11-09 14:26:36 +0300

    + StequeueNodeOfTests

---
+++ Source/UnitTests/RobertSedgewick/DataStructures/StequeNodeOfTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Algorithms.RobertSedgewick.Fundamentals.DataStructures;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.DataStructures
+{
+    public sealed class StequeNodeOfTests
+    {
+        [Fact]
+        public void IsEmpty_NewSteque_True()
+        {
+            var steque = new StequeNodeOf<int>();
+            Assert.True(steque.IsEmpty);
+        }
+    }
+}
