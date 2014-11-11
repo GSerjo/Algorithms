a1efa53ed3f0b74f569c7fe57bf794bd3369cbbf
GSerjo <sedot8@gmail.com>
2014-11-11 23:46:52 +0300

    + QueueNodeOfTests

---
+++ Source/UnitTests/RobertSedgewick/DataStructures/QueueNodeOfTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Algorithms.RobertSedgewick.Fundamentals.DataStructures;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.DataStructures
+{
+    public sealed class QueueNodeOfTests
+    {
+        [Fact]
+        public void Dequeue_EmptyQueue_ThrowException()
+        {
+            var queue = new QueueNodeOf<int>();
+            Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+        }
+
+        [Fact]
+        public void Dequeue_NewQueue_Ok()
+        {
+            var queue = new QueueNodeOf<int>();
+            queue.Enqueue(0)
+                 .Enqueue(1)
+                 .Enqueue(2);
+
+            Assert.Equal(0, queue.Dequeue());
+            Assert.Equal(1, queue.Dequeue());
+            Assert.Equal(2, queue.Dequeue());
+
+            Assert.Equal(0, queue.Count);
+            Assert.True(queue.IsEmpty);
+        }
+
+        [Fact]
+        public void Enqueue_NewQueue_Ok()
+        {
+            var queue = new QueueNodeOf<int>();
+            queue.Enqueue(0)
+                 .Enqueue(1);
+            Assert.Equal(2, queue.Count);
+            Assert.False(queue.IsEmpty);
+        }
+
+        [Fact]
+        public void IsEmpty_NewQueue_True()
+        {
+            var queue = new QueueNodeOf<int>();
+            Assert.True(queue.IsEmpty);
+            Assert.Equal(0, queue.Count);
+        }
+    }
+}
