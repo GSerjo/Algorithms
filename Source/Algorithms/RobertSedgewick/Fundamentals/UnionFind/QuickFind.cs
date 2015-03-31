8f272f4a03c9269ec0c6e5aa75a1d68c7114caf1
GSerjo <sedot8@gmail.com>
2015-03-31 23:12:41 +0300

    + QuickFind

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/UnionFind/QuickFind.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Algorithms.RobertSedgewick.Fundamentals.UnionFind
+{
+    public sealed class QuickFind
+    {
+        private readonly int[] _data;
+
+        public QuickFind(int count)
+        {
+            _data = new int[count];
+            Count = count;
+
+            for (int i = 0; i < count; i++)
+            {
+                _data[i] = i;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsConnected(int p, int q)
+        {
+            return Find(p) == Find(q);
+        }
+
+        public void Union(int p, int q)
+        {
+            int pId = Find(p);
+            int qId = Find(q);
+
+            if (pId == qId)
+            {
+                return;
+            }
+
+            for (int i = 0; i < _data.Length; i++)
+            {
+                if (_data[i] == pId)
+                {
+                    _data[i] = qId;
+                }
+            }
+            Count--;
+        }
+
+        private int Find(int p)
+        {
+            return _data[p];
+        }
+    }
+}
