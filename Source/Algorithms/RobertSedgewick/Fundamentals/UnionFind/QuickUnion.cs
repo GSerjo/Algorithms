8cc15fe0dc85a15a663a8b105bda2f1c8a96ef17
GSerjo <sedot8@gmail.com>
2015-04-02 01:50:14 +0300

    + QuickUnion

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/UnionFind/QuickUnion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Algorithms.RobertSedgewick.Fundamentals.UnionFind
+{
+    public sealed class QuickUnion
+    {
+        private readonly int[] _data;
+
+        public QuickUnion(int count)
+        {
+            _data = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                _data[i] = i;
+            }
+            Count = count;
+        }
+
+        public int Count { get; private set; }
+
+        public bool Connected(int p, int q)
+        {
+            return Find(p) == Find(q);
+        }
+
+        public void Union(int p, int q)
+        {
+            int pRoot = Find(p);
+            int qRoot = Find(q);
+
+            if (pRoot == qRoot)
+            {
+                return;
+            }
+            _data[pRoot] = qRoot;
+            Count--;
+        }
+
+        private int Find(int p)
+        {
+            while (p != _data[p])
+            {
+                p = _data[p];
+            }
+            return p;
+        }
+    }
+}
