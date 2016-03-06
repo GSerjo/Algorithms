963ea03e91d0f6724015ba7ea479de7b28446353
GSerjo <sedot8@gmail.com>
2016-03-05 18:59:58 -0800

    + QuickFind

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/UnionFind/QuickFind.cs
@@ -0,0 +1,48 @@
+﻿namespace Algorithms.RobertSedgewick.Fundamentals.UnionFind
+{
+    public sealed class QuickFind
+    {
+        private readonly int[] _ids;
+
+        public QuickFind(int length)
+        {
+            _ids = new int[length];
+            for (var i = 0; i < length; i++)
+            {
+                _ids[i] = i;
+            }
+            Count = length;
+        }
+
+        public int Count { get; private set; }
+
+        public void Union(int p, int q)
+        {
+            int idP = Find(p);
+            int idQ = Find(q);
+
+            if (idP == idQ)
+            {
+                return;
+            }
+            for (var i = 0; i < _ids.Length; i++)
+            {
+                if (_ids[i] == idP)
+                {
+                    _ids[i] = idQ;
+                }
+            }
+            Count--;
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return Find(p) == Find(q);
+        }
+
+        public int Find(int p)
+        {
+            return _ids[p];
+        }
+    }
+}
---
+++ Source/UnitTests/RobertSedgewick/Fundamentals/UnionFind/QuickFindTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Algorithms.RobertSedgewick.Fundamentals.UnionFind;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.Fundamentals.UnionFind
+{
+    public sealed class QuickFindTests
+    {
+        [Fact]
+        public void QuickFind_TinyFile_True()
+        {
+            var dots = new Dots(@"DataStore\tinyUF.txt");
+            var quickFind = new QuickFind(dots.Count);
+
+            foreach (Tuple<int, int> item in dots.Items)
+            {
+                if (quickFind.Connected(item.Item1, item.Item2))
+                {
+                    continue;
+                }
+                quickFind.Union(item.Item1, item.Item2);
+            }
+            Assert.Equal(2, quickFind.Count);
+        }
+
+
+        private sealed class Dots
+        {
+            public Dots(string filePath)
+            {
+                string[] content = File.ReadAllLines(filePath);
+                Count = int.Parse(content[0]);
+                Items = content.Skip(1)
+                              .Select(x => x.Split(null))
+                              .Select(x => Tuple.Create(int.Parse(x[0]), int.Parse(x[1])))
+                              .ToList();
+            }
+
+            public int Count { get; }
+            public List<Tuple<int, int>> Items { get; }
+        }
+    }
+}
