84e2f25cffbad3294d798d14bab5f970245406c4
GSerjo <sedot8@gmail.com>
2016-03-06 18:58:59 -0800

    + WeightedQuickUnion

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/UnionFind/WeightedQuickUnion.cs
@@ -0,0 +1,61 @@
+﻿namespace Algorithms.RobertSedgewick.Fundamentals.UnionFind
+{
+    public sealed class WeightedQuickUnion
+    {
+        private readonly int[] _ids;
+        private readonly int[] _sizes;
+
+        public WeightedQuickUnion(int length)
+        {
+            _ids = new int[length];
+            _sizes = new int[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                _ids[i] = i;
+                _sizes[i] = 1;
+            }
+            Count = length;
+        }
+
+        public int Count { get; private set; }
+
+        public bool Connected(int p, int q)
+        {
+            return Find(p) == Find(q);
+        }
+
+        public int Find(int p)
+        {
+            while (p != _ids[p])
+            {
+                p = _ids[p];
+            }
+            return p;
+        }
+
+        public void Union(int p, int q)
+        {
+            int pRoot = Find(p);
+            int qRoot = Find(q);
+
+            if (pRoot == qRoot)
+            {
+                return;
+            }
+
+            if (_sizes[pRoot] < _sizes[qRoot])
+            {
+                _ids[pRoot] = qRoot;
+                _sizes[qRoot] += _sizes[pRoot];
+            }
+            else
+            {
+                _ids[qRoot] = pRoot;
+                _sizes[pRoot] += _sizes[qRoot];
+            }
+
+            Count--;
+        }
+    }
+}
---
+++ Source/UnitTests/RobertSedgewick/Fundamentals/UnionFind/WeightedQuickUnionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Algorithms.RobertSedgewick.Fundamentals.UnionFind;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.Fundamentals.UnionFind
+{
+    public sealed class WeightedQuickUnionTests
+    {
+        [Fact]
+        public void QuickFind_TinyFile_True()
+        {
+            var dots = new Dots(@"DataStore\tinyUF.txt");
+            var weightedQuickUnion = new WeightedQuickUnion(dots.Count);
+
+            foreach (Tuple<int, int> item in dots.Items)
+            {
+                if (weightedQuickUnion.Connected(item.Item1, item.Item2))
+                {
+                    continue;
+                }
+                weightedQuickUnion.Union(item.Item1, item.Item2);
+            }
+            Assert.Equal(2, weightedQuickUnion.Count);
+        }
+    }
+}
