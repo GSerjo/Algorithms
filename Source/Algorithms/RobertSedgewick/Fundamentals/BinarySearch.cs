28c6c08db147f8c4f28291a5b5f9191196e65165
GSerjo <sedot8@gmail.com>
2016-03-21 20:21:44 -0700

    + BinarySearch

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿namespace Algorithms.RobertSedgewick.Fundamentals
+{
+    public static class BinarySearch
+    {
+        public static int Search(int key, int[] data)
+        {
+            if (data == null || data.Length == 0)
+            {
+                return -1;
+            }
+            var lo = 0;
+            int hi = data.Length - 1;
+
+            while (lo <= hi)
+            {
+                int med = lo + (hi - lo) / 2;
+                if (key > data[med])
+                {
+                    lo = med + 1;
+                }
+                else if (key < data[med])
+                {
+                    hi = med - 1;
+                }
+                else
+                {
+                    return med;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Source/UnitTests/RobertSedgewick/Fundamentals/BinarySearchTests.cs
@@ -0,0 +1,35 @@
+﻿using Algorithms.RobertSedgewick.Fundamentals;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.Fundamentals
+{
+    public sealed class BinarySearchTests
+    {
+        [Fact]
+        public void BinarySearch_Null_MinusOne()
+        {
+            int result = BinarySearch.Search(1, null);
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void BinarySearch_KeyExists_Ok()
+        {
+            var data = new[] { 1, 2, 3, 4, 5, 6 };
+
+            int result = BinarySearch.Search(3, data);
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void BinarySearch_KeyNotExists_Ok()
+        {
+            var data = new[] { 1, 2, 3, 4, 5, 6 };
+
+            int result = BinarySearch.Search(33, data);
+
+            Assert.Equal(-1, result);
+        }
+    }
+}
