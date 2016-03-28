8d307f05f0dc7b9a1d70849fb13fc2dc46eefc54
GSerjo <sedot8@gmail.com>
2016-03-27 18:31:57 -0700

    + String reverse

---
+++ Source/Algorithms/Exrensions/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Algorithms.Exrensions
+{
+    public static class StringExtensions
+    {
+        public static string Reverse(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return string.Empty;
+            }
+            var start = 0;
+            int end = value.Length - 1;
+            var result = new StringBuilder(value);
+            while (end > start)
+            {
+                char dummy = result[start];
+                result[start] = result[end];
+                result[end] = dummy;
+                end--;
+                start++;
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ Source/UnitTests/Exrensions/ReverseTests.cs
@@ -0,0 +1,17 @@
+﻿using Algorithms.Exrensions;
+using Xunit;
+
+namespace UnitTests.Exrensions
+{
+    public sealed class ReverseTests
+    {
+        [InlineData("abcd", "dcba")]
+        [InlineData("abcde", "edcba")]
+        [InlineData(null, "")]
+        [Theory]
+        public void Reverse_Value_Ok(string input, string expected)
+        {
+            Assert.Equal(expected, input.Reverse());
+        }
+    }
+}
