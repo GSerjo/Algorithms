d9f853db62b4b9b114f3d8426b81b93b38eea994
GSerjo <sedot8@gmail.com>
2016-03-28 19:16:15 -0700

    + Listextensions

---
+++ Source/Algorithms/Exrensions/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Exrensions
+{
+    public static class ListExtensions
+    {
+        public static void Shuffle<T>(this IList<T> collection)
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                
+            }
+        }
+    }
+}
