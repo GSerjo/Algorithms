faff8e11d14cc102f71505277ae2a82d35a0066a
GSerjo <sedot8@gmail.com>
2016-04-10 19:49:23 -0700

    + AllUniqueCharacters

---
+++ Source/Algorithms/Interview/Chapter1Questions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Interview
+{
+    public static class Chapter1Questions
+    {
+        /// <summary>
+        /// Implement an algorithm to determine if a string has all unique characters.
+        /// </summary>
+        public static bool AllUniqueCharacters(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return true;
+            }
+            var hash = new HashSet<char>();
+            foreach (char symbol in value)
+            {
+                if (hash.Contains(symbol))
+                {
+                    return false;
+                }
+                hash.Add(symbol);
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/UnitTests/Interview/Chapter1QuestionsTests.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Interview;
+using Xunit;
+
+namespace UnitTests.Interview
+{
+    public sealed class Chapter1QuestionsTests
+    {
+        [InlineData("", true)]
+        [InlineData(null, true)]
+        [InlineData("abcdef", true)]
+        [InlineData("abcde ff", false)]
+        [Theory]
+        public void AllUniqueCharacters(string value, bool expected)
+        {
+            bool result = Chapter1Questions.AllUniqueCharacters(value);
+            Assert.Equal(expected, result);
+        }
+    }
+}
