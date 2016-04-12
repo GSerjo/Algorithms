b56bda790adcf78a39c714dade22b8fa96015188
GSerjo <sedot8@gmail.com>
2016-04-11 18:30:05 -0700

    + SymbolTable

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/DataStructures/SymbolTable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Algorithms.RobertSedgewick.Fundamentals.DataStructures
+{
+    public sealed class SymbolTable<TKey, TValue>
+        where TKey : IComparable
+    {
+        private Node _first;
+
+        public TValue Get(TKey key)
+        {
+            Node node = Find(key);
+            return node == null ? default(TValue) : node.Value;
+        }
+
+        public void Put(TKey key, TValue value)
+        {
+            Node node = Find(key);
+            if (node == null)
+            {
+                _first = new Node { Key = key, Value = value, Next = _first };
+            }
+            else
+            {
+                node.Value = value;
+            }
+        }
+
+        private Node Find(TKey key)
+        {
+            Node node = _first;
+            while (node != null)
+            {
+                if (node.Key.Equals(key))
+                {
+                    return node;
+                }
+                node = node.Next;
+            }
+            return null;
+        }
+
+
+        private sealed class Node
+        {
+            public TKey Key { get; set; }
+            public Node Next { get; set; }
+            public TValue Value { get; set; }
+        }
+    }
+}
---
+++ Source/UnitTests/RobertSedgewick/DataStructures/SymbolTableTests.cs
@@ -0,0 +1,25 @@
+﻿using Algorithms.RobertSedgewick.Fundamentals.DataStructures;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.DataStructures
+{
+    public sealed class SymbolTableTests
+    {
+        [Fact]
+        public void Get()
+        {
+            var symbolTable = new SymbolTable<int, string>();
+
+            symbolTable.Put(1, "a");
+            symbolTable.Put(2, "b");
+
+            Assert.Equal("a", symbolTable.Get(1));
+
+            symbolTable.Put(1, "c");
+
+            Assert.Equal("c", symbolTable.Get(1));
+
+            Assert.Equal(null, symbolTable.Get(10));
+        }
+    }
+}
