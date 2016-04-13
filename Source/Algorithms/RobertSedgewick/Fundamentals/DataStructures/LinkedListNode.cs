7ad5a15edb7a366abb40f05ff683726c8102f13e
GSerjo <sedot8@gmail.com>
2016-04-12 18:04:44 -0700

    + LinkedListNode AddFirst and AddLast

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/DataStructures/LinkedListNode.cs
@@ -0,0 +1,50 @@
+﻿namespace Algorithms.RobertSedgewick.Fundamentals.DataStructures
+{
+    public sealed class LinkedListNode<T>
+    {
+        public Node Head { get; private set; }
+        public Node Tail { get; private set; }
+
+        public int Count { get; private set; }
+        public bool IsEmpty => Count == 0;
+
+        public void AddFirst(T value)
+        {
+            Node old = Head;
+            Head = new Node { Value = value };
+
+            if (IsEmpty)
+            {
+                Tail = Head;
+            }
+            else
+            {
+                Head.Next = old;
+            }
+            Count++;
+        }
+
+        public void AddLast(T value)
+        {
+            Node old = Tail;
+            Tail = new Node { Value = value };
+
+            if (IsEmpty)
+            {
+                Head = Tail;
+            }
+            else
+            {
+                old.Next = Tail;
+            }
+            Count++;
+        }
+
+
+        public sealed class Node
+        {
+            public Node Next { get; set; }
+            public T Value { get; set; }
+        }
+    }
+}
---
+++ Source/UnitTests/RobertSedgewick/DataStructures/LinkedListNodeTests.cs
@@ -0,0 +1,47 @@
+﻿using Algorithms.RobertSedgewick.Fundamentals.DataStructures;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.DataStructures
+{
+    public sealed class LinkedListNodeTests
+    {
+        [Fact]
+        public void Initial()
+        {
+            var linkedList = new LinkedListNode<int>();
+
+            Assert.Equal(0, linkedList.Count);
+            Assert.True(linkedList.IsEmpty);
+        }
+
+        [Fact]
+        public void AddFirst()
+        {
+            var linkedList = new LinkedListNode<int>();
+
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(2);
+
+            Assert.Equal(1, linkedList.Tail.Value);
+            Assert.Equal(2, linkedList.Head.Value);
+
+            Assert.False(linkedList.IsEmpty);
+            Assert.Equal(2, linkedList.Count);
+        }
+
+        [Fact]
+        public void AddLast()
+        {
+            var linkedList = new LinkedListNode<int>();
+
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+
+            Assert.Equal(2, linkedList.Tail.Value);
+            Assert.Equal(1, linkedList.Head.Value);
+
+            Assert.False(linkedList.IsEmpty);
+            Assert.Equal(2, linkedList.Count);
+        }
+    }
+}
