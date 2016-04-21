8b069e781958a921e41ec23a92bf10999d76c238
GSerjo <sedot8@gmail.com>
2016-04-20 18:50:16 -0700

    + DepthFirstPaths

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/DataStructures/DepthFirstPaths.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.RobertSedgewick.Fundamentals.DataStructures
+{
+    public sealed class DepthFirstPaths
+    {
+        private readonly int _sourceVertex;
+        private readonly bool[] _marked;
+        private readonly int[] _edgeTo;
+
+        public DepthFirstPaths(Graph graph, int sourceVertex)
+        {
+            _sourceVertex = sourceVertex;
+            _marked = new bool[graph.VertexCount];
+            _edgeTo = new int[graph.VertexCount];
+            DepthFirstSearch(graph, sourceVertex);
+        }
+
+        private void DepthFirstSearch(Graph graph, int vertex)
+        {
+            _marked[vertex] = true;
+            foreach (int w in graph.Adj(vertex))
+            {
+                if (_marked[w])
+                {
+                    continue;
+                }
+                _edgeTo[w] = vertex;
+                DepthFirstSearch(graph, w);
+            }
+        }
+
+        public bool HasPathTo(int vertex)
+        {
+            return _marked[vertex];
+        }
+
+        public IEnumerable<int> PathTo(int vertex)
+        {
+            if (!HasPathTo(vertex))
+            {
+                return Enumerable.Empty<int>();
+            }
+            int current = _edgeTo[vertex];
+            var result = new Stack<int>(new[] { current });
+
+            while (_sourceVertex != current)
+            {
+                current = _edgeTo[current];
+                result.Push(current);
+            }
+            return result;
+        }
+
+    }
+}
--- Source/Algorithms/RobertSedgewick/Fundamentals/DataStructures/Graph.cs
+++ Source/Algorithms/RobertSedgewick/Fundamentals/DataStructures/Graph.cs
@@ -6,16 +6,18 @@ namespace Algorithms.RobertSedgewick.Fundamentals.DataStructures
     {
         private readonly List<int>[] _adj;
 
-        public Graph(int vertex)
+        public Graph(int vertexCount)
         {
-            _adj = new List<int>[vertex];
-            for (var i = 0; i < vertex; i++)
+            VertexCount = vertexCount;
+            _adj = new List<int>[vertexCount];
+            for (var i = 0; i < vertexCount; i++)
             {
                 _adj[i] = new List<int>();
             }
         }
 
         public int EdgeCount { get; private set; }
+        public int VertexCount { get; }
 
         public void AddEdge(int vertexOne, int vertexSecond)
         {
