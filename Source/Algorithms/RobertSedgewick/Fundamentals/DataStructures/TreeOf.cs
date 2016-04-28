728a62ecbd0dee6881bef28b071e3ec988a14873
admlan\morens <smorenko@gmail.com>
2016-04-28 17:11:39 -0400

    + TreeOf

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/DataStructures/TreeOf.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Algorithms.RobertSedgewick.Fundamentals.DataStructures
+{
+    public sealed class TreeOf<T>
+        where T : IComparable
+    {
+        private Node _root;
+
+        public void PrintInOrder(Action<T> action)
+        {
+            PrintInOrder(_root, action);
+        }
+
+        public void Put(T value)
+        {
+            _root = Put(_root, value);
+        }
+
+        private static void PrintInOrder(Node node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            PrintInOrder(node.Left, action);
+            action(node.Value);
+            PrintInOrder(node.Right, action);
+        }
+
+        private static Node Put(Node node, T value)
+        {
+            if (node == null)
+            {
+                return new Node { Value = value };
+            }
+            int compare = value.CompareTo(node.Value);
+            if (compare > 0)
+            {
+                node.Right = Put(node.Right, value);
+            }
+            else if (compare < 0)
+            {
+                node.Left = Put(node.Left, value);
+            }
+            else
+            {
+                node.Value = value;
+            }
+            return node;
+        }
+
+
+        private sealed class Node
+        {
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public T Value { get; set; }
+        }
+    }
+}
---
+++ Source/UnitTests/RobertSedgewick/DataStructures/TreeOfTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Algorithms.RobertSedgewick.Fundamentals.DataStructures;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.DataStructures
+{
+    public sealed class TreeOfTests
+    {
+        [Fact]
+        public void PrintInOrder()
+        {
+            var tree = new TreeOf<int>();
+            tree.Put(5);
+            tree.Put(7);
+            tree.Put(4);
+
+            var result = new List<int>();
+            tree.PrintInOrder(result.Add);
+
+            Assert.Equal(new[] { 4, 5, 7 }, result);
+        }
+    }
+}
