e18f370984fde7656ec1f379b0eaa72823fbc86b
GSerjo <sedot8@gmail.com>
2016-05-01 09:49:53 -0700

    + LongestIncreasingSubsequence

---
+++ Source/Algorithms/DynamicProgramming/LongestIncreasingSubsequence.cs
@@ -0,0 +1,37 @@
+﻿namespace Algorithms.DynamicProgramming
+{
+    public static class LongestIncreasingSubsequence
+    {
+        public static int FindLength(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                return 0;
+            }
+
+            var subSequenceCount = new int[array.Length];
+            var result = 0;
+            for (var i = 0; i < array.Length; i++)
+            {
+                subSequenceCount[i] = 1;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (array[j] > array[i])
+                    {
+                        continue;
+                    }
+                    if (subSequenceCount[j] + 1 > subSequenceCount[i])
+                    {
+                        subSequenceCount[i] = subSequenceCount[j] + 1;
+                    }
+                }
+                if (subSequenceCount[i] > result)
+                {
+                    result = subSequenceCount[i];
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Source/UnitTests/DynamicProgramming/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,17 @@
+﻿using Algorithms.DynamicProgramming;
+using Xunit;
+
+namespace UnitTests.DynamicProgramming
+{
+    public sealed class LongestIncreasingSubsequenceTests
+    {
+        [Fact]
+        public void FindLength()
+        {
+            var array = new[] { 7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1 };
+            int result = LongestIncreasingSubsequence.FindLength(array);
+
+            Assert.Equal(5, result);
+        }
+    }
+}
