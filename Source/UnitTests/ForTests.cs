4f4f5bf50d157b7dcbbbea94cfd9f8bbd00f5061
GSerjo <sedot8@gmail.com>
2016-05-09 10:05:57 -0400

    balanced parentheses initial value

--- Source/UnitTests/ForTests.cs
+++ Source/UnitTests/ForTests.cs
@@ -7,6 +7,33 @@ namespace UnitTests
         [Fact]
         public void Test()
         {
+            var result = GetValue(")()(");
+            result = GetValue("()()");
+            result = GetValue("((())");
+        }
+
+        private static bool GetValue(string value)
+        {
+            var openPretences = 0;
+            var closedPretences = 0;
+
+            foreach (char item in value)
+            {
+                if (item == ')' && closedPretences >= openPretences)
+                {
+                    return false;
+                }
+                if (item == '(')
+                {
+                    openPretences++;
+                    continue;
+                }
+                if (openPretences > closedPretences && item == ')')
+                {
+                    closedPretences++;
+                }
+            }
+            return openPretences == closedPretences;
         }
     }
 }
