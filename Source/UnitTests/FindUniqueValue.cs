e98001c506dfa4aa482cb381d8643b83af95f55b
GSerjo <sedot8@gmail.com>
2016-05-08 20:04:38 -0700

    + FindUniqueValue

---
+++ Source/UnitTests/FindUniqueValue.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace UnitTests
+{
+    public sealed class FindUniqueValue
+    {
+        [Fact]
+        public void HasUnique()
+        {
+            var array = new[] { 1, 2, 3, 4, 1, 2, 3, 4, 5 };
+            int unique = FindUnique(array);
+            Assert.Equal(5, unique);
+        }
+
+        private static int FindUnique(int[] array)
+        {
+            int result = array[0];
+
+            for (var i = 1; i < array.Length; i++)
+            {
+                result = result ^ array[i];
+            }
+            return result;
+        }
+    }
+}
