577163339f88a1ce39c02f5810ae92681cbaa61a
GSerjo <sedot8@gmail.com>
2016-05-18 09:59:39 -0400

    + QueueNodeOf

---
+++ Source/Algorithms/RobertSedgewick/Fundamentals/DataStructures/QueueNodeOf.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Algorithms.RobertSedgewick.Fundamentals.DataStructures
+{
+    public sealed class QueueNodeOf<T>
+    {
+        private Node _head;
+        private Node _tail;
+
+        public int Count { get; private set; }
+        public bool IsEmpty => Count == 0;
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException();
+            }
+            T result = _head.Value;
+            _head = _head.Next;
+            Count--;
+            return result;
+        }
+
+        public void Enqueue(T value)
+        {
+            Node oldTail = _tail;
+            _tail = new Node { Value = value };
+            if (IsEmpty)
+            {
+                _head = _tail;
+            }
+            else
+            {
+                oldTail.Next = _tail;
+            }
+            Count++;
+        }
+
+
+        private sealed class Node
+        {
+            public Node Next { get; set; }
+            public T Value { get; set; }
+        }
+    }
+}
---
+++ Source/UnitTests/RobertSedgewick/DataStructures/QueueNodeOfTests.cs
@@ -0,0 +1,24 @@
+﻿using Algorithms.RobertSedgewick.Fundamentals.DataStructures;
+using Xunit;
+
+namespace UnitTests.RobertSedgewick.DataStructures
+{
+    public sealed class QueueNodeOfTests
+    {
+        [Fact]
+        public void Dequeue()
+        {
+            var queue = new QueueNodeOf<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Assert.Equal(3, queue.Count);
+
+            Assert.Equal(1, queue.Dequeue());
+            Assert.Equal(2, queue.Dequeue());
+            Assert.Equal(3, queue.Dequeue());
+        }
+    }
+}
