09c31e05c40a73ec5ec89e8767332b1e75e6340c
GSerjo <sedot8@gmail.com>
2017-05-25 17:56:43 -0400

    + tests initial

---
+++ Source/Algorithms/Algorithms-Part-I/W2/tests/DequeTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Serjo on 5/25/2017.
+ */
+class DequeTest {
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void addFirst() {
+    }
+
+    @Test
+    void removeFirst() {
+    }
+
+    @Test
+    void addLast() {
+    }
+
+    @Test
+    void removeLast() {
+    }
+
+}
