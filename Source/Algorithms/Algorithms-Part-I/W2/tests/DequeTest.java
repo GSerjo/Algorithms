4df50cb178c4d55264aa474bf57eb0b4b2588250
GSerjo <sedot8@gmail.com>
2017-05-25 20:58:32 -0400

    * dequeTests

--- Source/Algorithms/Algorithms-Part-I/W2/tests/DequeTest.java
+++ Source/Algorithms/Algorithms-Part-I/W2/tests/DequeTest.java
@@ -3,31 +3,41 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Created by Serjo on 5/25/2017.
+ * Created by Serjo on 25/05/2017.
  */
 class DequeTest {
-    @Test
-    void size() {
-    }
 
     @Test
     void isEmpty() {
+        assertEquals(0, new Deque<Integer>().size());
     }
 
     @Test
-    void addFirst() {
-    }
+    void addFirstAndRemoveFirst() {
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
 
-    @Test
-    void removeFirst() {
+        assertEquals(2, deque.removeFirst().intValue());
+        assertEquals(1, deque.size());
+        assertFalse(deque.isEmpty());
+
+        assertEquals(1, deque.removeLast().intValue());
+        assertTrue(deque.isEmpty());
     }
 
     @Test
     void addLast() {
-    }
+        Deque<Integer> deque = new Deque<>();
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
 
-    @Test
-    void removeLast() {
-    }
+        assertEquals(1, deque.removeFirst().intValue());
+        assertEquals(2, deque.size());
 
+        assertEquals(3, deque.removeLast().intValue());
+        assertEquals(1, deque.size());
+        assertFalse(deque.isEmpty());
+    }
 }
