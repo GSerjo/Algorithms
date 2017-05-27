75f45ac832bd19bb903458810086c3e81079d857
GSerjo <sedot8@gmail.com>
2017-05-27 17:31:35 -0400

    + permutation

--- Source/Algorithms/Algorithms-Part-I/W2/src/Permutation.java
+++ Source/Algorithms/Algorithms-Part-I/W2/src/Permutation.java
@@ -1,8 +1,26 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
 
-    public static void maint(String[] args){
+    public static void main(String[] args){
 
-
+        int k = Integer.parseInt(args[0]);
+        String[] items = StdIn.readAllStrings();
         RandomizedQueue<String> queue = new RandomizedQueue<>();
+        int rest = items.length - k;
+
+        for (int i = 0; i < k; i++){
+            queue.enqueue(items[i]);
+        }
+
+        for (int i = 0; i < k; i++){
+            StdOut.print(queue.dequeue());
+            if(rest > 0){
+                queue.enqueue(items[k + rest - 1]);
+                rest--;
+            }
+        }
+        
     }
 }
