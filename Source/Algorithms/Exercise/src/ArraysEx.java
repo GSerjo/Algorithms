49b1755840b51a0516ebbf67d130d0cbb0ffcbd9
GSerjo <sedot8@gmail.com>
2017-07-19 23:25:55 -0400

    + isUniqueChar 

--- Source/Algorithms/Exercise/src/ArraysEx.java
+++ Source/Algorithms/Exercise/src/ArraysEx.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+
 public class ArraysEx {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println(isUniqueChar("tes"));
+    }
 
+    private static boolean isUniqueChar(String value) {
+        if (value == null || value.equals("") || value.length() == 1) {
+            return true;
+        }
+        char[] array = value.toCharArray();
+        Arrays.sort(array);
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] == array[i-1]){
+                return false;
+            }
+        }
+        return true;
     }
 }
