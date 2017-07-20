1ac61b95ff00287b79561be77c9965b969caac38
GSerjo <sedot8@gmail.com>
2017-07-20 00:08:28 -0400

    + isPermutation

--- Source/Algorithms/Exercise/src/ArraysEx.java
+++ Source/Algorithms/Exercise/src/ArraysEx.java
@@ -3,7 +3,32 @@ import java.util.Arrays;
 public class ArraysEx {
 
     public static void main(String[] args) {
+
+        System.out.println(isPermutation("tes", "set"));
+
+
         System.out.println(isUniqueChar("tes"));
+    }
+
+
+    private static boolean isPermutation(String a, String b) {
+        if (a == null || b == null || a.length() != b.length()) {
+            return false;
+        }
+        if (a.equals(b)) {
+            return true;
+        }
+        char[] arrayA = a.toCharArray();
+        char[] arrayB = b.toCharArray();
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+
+        for (int i = 0; i < arrayA.length; i++) {
+            if (arrayA[i] != arrayB[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static boolean isUniqueChar(String value) {
@@ -13,7 +38,7 @@ public class ArraysEx {
         char[] array = value.toCharArray();
         Arrays.sort(array);
         for (int i = 1; i < array.length; i++) {
-            if(array[i] == array[i-1]){
+            if (array[i] == array[i - 1]) {
                 return false;
             }
         }
